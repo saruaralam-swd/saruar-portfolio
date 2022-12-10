0f6e21c91fc5e8da976f061537d33eeb5ff66848
saruaralam-swd <saruaralam2001@gmail.com>
2022-12-10 21:27:07 +0600

    contact form add

---
+++ src/Pages/Contact/Contact.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import img from '../../assets/my-img.jpeg';
+
+const Contact = () => {
+  return (
+    <div className='my-10'>
+      <h2 className='text-center text-4xl underline'>Get in Touch</h2>
+      
+      <div className='grid md:grid-cols-2 lg:grid-cols-3 mt-10'>
+        <div className='p-4 md:pl-0'>
+          <img className='sm:w-3/4 border-2 border-indigo-500 rounded-md' src={img} alt="" />
+        </div>
+
+        <div className=' p-4 lg:col-span-2'>
+          <h2 className='text-2xl font-semibold'>Contact Me</h2>
+          <form className='mt-5'>
+            <div className='space-y-5'>
+              <div>
+                <label htmlFor="name">Name</label>
+                <input className='border block w-full px-4 py-2 rounded-md' type="text" placeholder='full Name' />
+              </div>
+
+              <div>
+                <label htmlFor="name">Email</label>
+                <input className='border block w-full px-4 py-2 rounded-md' type="email" placeholder='example@gmail.com' />
+              </div>
+
+              <div>
+                <div>
+                  <label htmlFor="name">Message</label>
+                </div>
+                <textarea className="textarea-bordered w-full textarea" placeholder="Bio"></textarea>
+              </div>
+
+              <button className='w-[100px] border px-4 py-1 rounded-full bg-indigo-500 hover:bg-indigo-600 text-white duration-500'>Send</button>
+            </div>
+
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
--- src/Routes/Routes/Routes.js
+++ src/Routes/Routes/Routes.js
@@ -1,5 +1,6 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../../Layout/Main";
+import Contact from "../../Pages/Contact/Contact";
 import Home from "../../Pages/Home/Home/Home";
 
 export const router = createBrowserRouter([
@@ -10,7 +11,12 @@ export const router = createBrowserRouter([
       {
         path: '/',
         element: <Home></Home>
+      },
+      {
+        path: '/contact',
+        element: <Contact></Contact>
       }
     ]
-  }
+  },
+  
 ]);
