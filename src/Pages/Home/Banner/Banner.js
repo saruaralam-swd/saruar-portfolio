9d4f0fe99f062e6ef7123f08f814f69e61688ca7
saruaralam-swd <saruaralam2001@gmail.com>
2023-01-15 01:05:57 +0600

    add social icons color

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -7,7 +7,7 @@ import { BsFacebook, BsGithub, BsLinkedin } from "react-icons/bs";
 const Banner = () => {
   return (
     <div className='min-h-[500px] md:flex items-center p-5'>
-      <div className='md:w-1/2 space-y-3 pl-14 text-center md:text-left lg:text-left'>
+      <div className='md:w-1/2 space-y-3 pl-14 text-center md:text-left lg:text-left py-10 md:py-0 '>
         <p className='text-xl'>Hi, I'm</p>
         <p className='text-4xl md:text-5xl font-semibold'>Md. Saruar Alam</p>
         <p className='font-semibold text-[#6366F1] text-2xl'>
@@ -21,9 +21,9 @@ const Banner = () => {
         </p>
         
         {/* social icons */}
-        <a href="https://web.facebook.com/saruaralam2001" target="_blank" className='inline-block' rel="noreferrer"><BsFacebook className='h-7 w-7' /></a>
-        <a href="https://github.com/saruaralam-swd" target="_blank" className='inline-block mx-4' rel="noreferrer"><BsGithub className='h-7 w-7' /></a>
-        <a href="https://www.linkedin.com/in/saruaralam/" target="_blank" className='inline-block' rel="noreferrer"><BsLinkedin className='h-7 w-7 rounded-full' /></a>
+        <a href="https://web.facebook.com/saruaralam2001" target="_blank" className='inline-block bg-white text-black text-[#3b5998] rounded-full' rel="noreferrer"><BsFacebook className='h-7 w-7' /></a>
+        <a href="https://github.com/saruaralam-swd" target="_blank" className='inline-block bg-white text-black  rounded-full mx-4' rel="noreferrer"><BsGithub className='h-7 w-7' /></a>
+        <a href="https://www.linkedin.com/in/saruaralam/" target="_blank" className='inline-block bg-white text-black text-[#0072b1] rounded-full' rel="noreferrer"><BsLinkedin className='h-7 w-7 rounded-full' /></a>
         <a href='https://form.jotform.com/223434222605042' className='block'><button className='border px-4 py-2 rounded-full bg-indigo-500 text-white hover:bg-white hover:border-indigo-500 hover:text-black duration-500'>Contact me</button></a>
       </div>
 
