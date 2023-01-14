b8be23d30aa4c482006473421f532648befac523
saruaralam-swd <saruaralam2001@gmail.com>
2023-01-15 00:54:46 +0600

    update icons

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -2,24 +2,29 @@ import React from 'react';
 import './Banner.css'
 import img from '../../../assets/my-img.jpeg';
 import Typewriter from 'typewriter-effect';
+import { BsFacebook, BsGithub, BsLinkedin } from "react-icons/bs";
 
 const Banner = () => {
   return (
     <div className='min-h-[500px] md:flex items-center p-5'>
-      <div className='md:w-1/2 space-y-2'>
-        <p className='text-lg'>Hey There !</p>
-        <p className='text-4xl md:text-5xl  font-semibold'>I am Saruar Alam</p>
-        <p className='font-semibold text-indigo-500 text-lg'>
+      <div className='md:w-1/2 space-y-3 pl-14 text-center md:text-left lg:text-left'>
+        <p className='text-xl'>Hi, I'm</p>
+        <p className='text-4xl md:text-5xl font-semibold'>Md. Saruar Alam</p>
+        <p className='font-semibold text-[#6366F1] text-2xl'>
           <Typewriter
             options={{
-              strings: ['React Developer', 'MERN stack developer'],
+              strings: ['React js Developer', 'MERN Stack Developer', 'Frontend Developer'],
               autoStart: true,
               loop: true,
             }}
           />
         </p>
         
-        <a href='https://form.jotform.com/223434222605042' className='inline-block'><button className='border px-4 py-1 rounded-full bg-indigo-500 text-white hover:bg-white hover:border-indigo-500 hover:text-black duration-500'>Contact me</button></a>
+        {/* social icons */}
+        <a href="https://web.facebook.com/saruaralam2001" target="_blank" className='inline-block' rel="noreferrer"><BsFacebook className='h-7 w-7' /></a>
+        <a href="https://github.com/saruaralam-swd" target="_blank" className='inline-block mx-4' rel="noreferrer"><BsGithub className='h-7 w-7' /></a>
+        <a href="https://www.linkedin.com/in/saruaralam/" target="_blank" className='inline-block' rel="noreferrer"><BsLinkedin className='h-7 w-7 rounded-full' /></a>
+        <a href='https://form.jotform.com/223434222605042' className='block'><button className='border px-4 py-2 rounded-full bg-indigo-500 text-white hover:bg-white hover:border-indigo-500 hover:text-black duration-500'>Contact me</button></a>
       </div>
 
       <div className='w-1/2 mx-auto mt-10'>
