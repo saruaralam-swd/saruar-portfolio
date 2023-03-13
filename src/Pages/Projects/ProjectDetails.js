830271283c51576bf1115e7779f8ba86b7b5f5c4
saruaralam-swd <saruaralam2001@gmail.com>
2023-03-13 23:19:18 +0600

    project image modify

--- src/Pages/Projects/ProjectDetails.js
+++ src/Pages/Projects/ProjectDetails.js
@@ -4,6 +4,7 @@ import { useLoaderData } from 'react-router-dom';
 const ProjectDetails = () => {
   const project = useLoaderData();
   const { title, des, features, github, images, live, techs } = project;
+  console.log(images);
 
   return (
     <div className='py-10 p-5 bg-slate-100 rounded-md'>
@@ -19,12 +20,14 @@ const ProjectDetails = () => {
         <a href={github} target='_blank' className='text-xl font-semibold text-indigo-500 ml-2' rel="noreferrer">Github</a>
       </div>
       <div className="divider"></div>
-      <div className='grid md:grid-cols-3 gap-5'>
+      <div className='px-5 md:px-10'>
         {
-          images.map((img, index) => <div key={index} className='mt-10'>
-            <h2 className=' bg-slate-500 text-white w-[30px] h-[30px] rounded-full flex justify-center items-center mb-1'>{index + 1}</h2>
-            <img src={`${img}`} className='rounded-md' alt='project images' />
-          </div>)
+          images.map((img, index) =>
+            <div key={index} className='mt-10'>
+              <h2 className=' bg-slate-500 text-white w-[30px] h-[30px] rounded-full flex justify-center items-center mb-1'> {index + 1}</h2>
+              <img src={`${img}`} className='rounded-md' alt='project images' />
+            </div>
+          )
         }
       </div>
     </div>
