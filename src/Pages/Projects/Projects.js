2ecaa0ab6cedab5f5986dcca946028cd364a4d9f
saruaralam-swd <saruaralam2001@gmail.com>
2023-05-10 20:59:34 +0600

    projects loading add

--- src/Pages/Projects/Projects.js
+++ src/Pages/Projects/Projects.js
@@ -1,18 +1,78 @@
 import React, { useEffect, useState } from 'react';
 import { FaExternalLinkAlt, FaGithub } from "react-icons/fa";
-import { Link, useLoaderData } from 'react-router-dom';
-import useProjects from '../../hooks/useProjects';
+import { Link } from 'react-router-dom';
 import useTitle from '../../hooks/useTitle';
 
 const Projects = () => {
   useTitle('Projects');
-  const [projects, projectsLoader] = useProjects();
+  const [projectsLoader, setProjectsLoader] = useState(true);
+  const [projects, setProjects] = useState([]);
+
+  useEffect(() => {
+    fetch('https://saruar-portfolio-server.vercel.app/projects')
+      .then(res => res.json())
+      .then(data => {
+        setProjectsLoader(false);
+        setProjects(data)
+      })
+  }, []);
 
   return (
     <div className='my-10 px-5'>
       <h2 className="text-3xl font-semibold mb-5">My Projects</h2>
+
       {
-        projectsLoader && <div className='text-center text-5xl'>Loading...</div>
+        projectsLoader && <>
+          <div className='grid md:grid-cols-3 gap-5'>
+            <div class="border border-indigo-200 shadow rounded-md p-4 max-w-sm h-[250px] w-full ">
+              <div class="animate-pulse flex space-x-4">
+                <div class="flex-1 space-y-6 py-1">
+                  <div class="rounded-md bg-slate-200 h-28 w-full"></div>
+                  <div class="h-2 bg-slate-200 rounded"></div>
+                  <div class="space-y-3">
+                    <div class="grid grid-cols-3 gap-4">
+                      <div class="h-2 bg-slate-200 rounded col-span-2"></div>
+                      <div class="h-2 bg-slate-200 rounded col-span-1"></div>
+                    </div>
+                    <div class="h-2 bg-slate-200 rounded"></div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div class="border border-indigo-200 shadow rounded-md p-4 max-w-sm h-[250px] w-full ">
+              <div class="animate-pulse flex space-x-4">
+                <div class="flex-1 space-y-6 py-1">
+                  <div class="rounded-md bg-slate-200 h-28 w-full"></div>
+                  <div class="h-2 bg-slate-200 rounded"></div>
+                  <div class="space-y-3">
+                    <div class="grid grid-cols-3 gap-4">
+                      <div class="h-2 bg-slate-200 rounded col-span-2"></div>
+                      <div class="h-2 bg-slate-200 rounded col-span-1"></div>
+                    </div>
+                    <div class="h-2 bg-slate-200 rounded"></div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div class="border border-indigo-200 shadow rounded-md p-4 max-w-sm h-[250px] w-full ">
+              <div class="animate-pulse flex space-x-4">
+                <div class="flex-1 space-y-6 py-1">
+                  <div class="rounded-md bg-slate-200 h-28 w-full"></div>
+                  <div class="h-2 bg-slate-200 rounded"></div>
+                  <div class="space-y-3">
+                    <div class="grid grid-cols-3 gap-4">
+                      <div class="h-2 bg-slate-200 rounded col-span-2"></div>
+                      <div class="h-2 bg-slate-200 rounded col-span-1"></div>
+                    </div>
+                    <div class="h-2 bg-slate-200 rounded"></div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </>
       }
 
       <div className='grid md:grid-cols-3 gap-10'>
