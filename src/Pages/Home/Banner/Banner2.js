d25b1e3ebb9184d0183e8476cf796ebe39e9e788
saruaralam-swd <saruaralam2001@gmail.com>
2025-08-28 02:21:42 +0600

    update banner background image and remove unused image file

--- src/Pages/Home/Banner/Banner2.js
+++ src/Pages/Home/Banner/Banner2.js
@@ -1,4 +1,4 @@
-import bannerImg from "../../../assets/banner/bg.jpg";
+import bannerBg from "../../../assets/banner/bg.jpg";
 import "../Banner/Banner2.css";
 import Banner2Header from "./Banner2Header";
 import BannerText from "./BannerText";
@@ -6,8 +6,8 @@ import BannerText from "./BannerText";
 const Banner2 = () => {
   return (
     <div
+      style={{ backgroundImage: `url(${bannerBg})` }}
       className="banner-container relative text-gray-400 "
-      style={{ backgroundImage: `url(${bannerImg})` }}
     >
       <div className="banner-img"></div>
       <Banner2Header />
